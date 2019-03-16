d421095394917b20a869282bd5824d183d9ff035
Shane D Wilson <me@shanedwilson.com>
2019-03-16 09:00:03 -0500

    Add project files.

---
+++ FavoriteToppings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FavoriteToppings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
