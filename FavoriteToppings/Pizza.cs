354dceea5c3ec145196d8b9db397db1df4a5e8e8
Shane D Wilson <me@shanedwilson.com>
2019-03-18 16:10:27 -0500

    working

---
+++ FavoriteToppings/Pizza.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FavoriteToppings
+{
+    class Pizza
+    {
+        public List<string> Toppings { get; set; }
+
+        public override string ToString()
+        {
+            return string.Join(", ", Toppings);
+        }
+
+
+
+    }
+}
--- FavoriteToppings/Program.cs
+++ FavoriteToppings/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace FavoriteToppings
 {
@@ -6,7 +10,23 @@ namespace FavoriteToppings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var allThePizzas = File.ReadAllText(@"C:\Users\Shane D Wilson\source\repos\FavoriteToppings\pizzas.json");
+            List<Pizza> allPizzaOrders = JsonConvert.DeserializeObject<List<Pizza>>(allThePizzas);
+
+            var orderedPizzas = allPizzaOrders
+                .Select(pizza => pizza.ToString())
+                .GroupBy(pizza => pizza)
+                .OrderByDescending(toppings => toppings.Count())
+                .Select(s => new { s.Key, Number = s.Count() });
+
+            var top20 = orderedPizzas.Take(20);
+
+            foreach (var pizza in top20)
+            {
+                Console.WriteLine($"{pizza.Key} occurs {pizza.Number} times");
+            }
+
+            Console.ReadKey();
         }
     }
 }
